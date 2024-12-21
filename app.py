2516ee2cd30f09fd88bd5005f4bfe7f9e1434e93
BenIsom20 <158601952+BenIsom20@users.noreply.github.com>
2024-12-21 14:13:43 -0600

    Made initialization of columns and text more efficient with new app.py function

--- app.py
+++ app.py
@@ -70,5 +70,29 @@ def test_code():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+@app.route("/Startup", methods=["GET"])
+def Startup():
+    # Example explanation for the test
+    descriptions = {
+        "Question": "Write a method to print 12345",
+        "test1ex": "Test 1: Checking if 1 is present",
+        "test2ex": "Test 2: Checking if 2 is present",
+        "test3ex": "Test 3: Checking if 3 is present",
+        "test4ex": "Test 4: Checking if 4 is present",
+        "test5ex": "Test 5: Checking if 5 is present"
+    }
+
+    explanation = ""
+    for value in descriptions.values():
+        explanation += value + "<br>" 
+    # Add the value to the total
+
+    
+    return jsonify({
+        "explanation": explanation,
+        "Array": len(descriptions)-1
+    })
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
